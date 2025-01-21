ccb95680ed39bed1042f11374f7f9477416df5cd
Kennn05 <bosslevelgod2@gmail.com>
2025-01-21 11:33:14 +0800

    first commit

--- filecrypt.sh
+++ filecrypt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+clear
+echo ""
+ls
+echo "
+"
 # Check if OpenSSL is installed
 if ! command -v openssl &> /dev/null; then
   echo "Error: OpenSSL is not installed. Please install it and try again."
@@ -9,9 +13,11 @@ fi
 # Function to display menu
 display_menu() {
   echo "Select an option:"
-  echo "1) Encrypt a file"
-  echo "2) Decrypt a file"
-  echo "0) Exit"
+  echo "----------------------"
+  echo "[1] Encrypt a file"
+  echo "[2] Decrypt a file"
+  echo "[0] Exit"
+  echo ""
   read -p "Enter your choice: " choice
 }
 
@@ -26,9 +32,10 @@ encrypt_file() {
   output_file="${input_file}.enc"
   read -sp "Enter password: " password
   echo
-  openssl enc -aes-256-cbc -salt -in "$input_file" -out "$output_file" -pass pass:"$password"
+  openssl enc -aes-256-cbc -salt -in "$input_file" -out "$output_file" -pass pass:"$password" >/dev/null 2>&1
 
   if [ $? -eq 0 ]; then
+  	echo ""
     echo "File encrypted successfully: $output_file"
     exit 1
   else
@@ -53,7 +60,7 @@ decrypt_file() {
   output_file="${input_file%.enc}"
   read -sp "Enter password: " password
   echo
-  openssl enc -aes-256-cbc -d -salt -in "$input_file" -out "$output_file" -pass pass:"$password"
+  openssl enc -aes-256-cbc -d -salt -in "$input_file" -out "$output_file" -pass pass:"$password" >/dev/null 2>&1
 
   if [ $? -eq 0 ]; then
     echo "File decrypted successfully: $output_file"
