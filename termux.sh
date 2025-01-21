b89277ce6fe3ee983c54d55d42806bca19b4280c
Kennn05 <bosslevelgod2@gmail.com>
2025-01-21 12:54:58 +0800

    first commit

--- termux.sh
+++ termux.sh
@@ -1,5 +1,5 @@
-cp filecrypt.sh ../../usr/bin/filecrypt
-chmod +x ../../usr/bin/filecrypt 
+cp $HOME/filecrypt/filecrypt.sh $HOME/../usr/bin/filecrypt
+chmod +x $HOME/../usr/bin/filecrypt 
 clear 
 echo " SUCCESSFUL type 'filecrypt' to use the tool"
 echo "rm -f $HOME/../usr/bin/filecrypt && rm -rf $HOME/filecrypt && git clone https://github.com/kennn05/filecrypt && bash $HOME/filecrypt/termux.sh " > ~/../usr/bin/update
