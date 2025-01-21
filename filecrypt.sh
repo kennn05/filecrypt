e2a89e8ad150d75dc1b48980e3263dbb55739b86
Kennn05 <bosslevelgod2@gmail.com>
2025-01-21 12:31:19 +0800

    first commit

--- filecrypt.sh
+++ filecrypt.sh
@@ -4,49 +4,53 @@ RED='\033[0;31m'
 GREEN='\033[1;92m'
 NC='\033[0m'
 
-echo -e """${RED}
-     .--------.       
-    / .------. \       
-   / /        \ \    
-   | |        | |     
-  _| |________| |_    
-.' |_|        |_| '.
-'._____ ____ _____.'
-|     .'____'.     |
-'.__.'.'    '.'.__.'
-'.__  |CRYPTO|   _.'
-|   '.'.____.'.'   |
-'.____'.____.'____.'
-'.________________.'"""
-echo ""
-echo "Coded by Ken"
-echo -e "------------------------------------${NC}"
-ls 
-echo -e "${RED}------------------------------------"
-echo ""
+banner() { 
+	echo -e """${RED}
+  	   .--------.       
+	  / .------. \       
+     / /        \ \    
+     | |        | |     
+    _| |________| |_    
+  .' |_|        |_| '.
+  '._____ ____ _____.'
+  |     .'____'.     |
+  '.__.'.'    '.'.__.'
+  '.__  |CRYPTO|   _.'
+  |   '.'.____.'.'   |
+  '.____'.____.'____.'
+  '.________________.'"""
+ 	echo ""
+    echo "Coded by Ken"
+    echo -e "------------------------------------${NC}"
+    ls 
+    echo -e "${RED}------------------------------------"
+}
+
 # Check if OpenSSL is installed
 if ! command -v openssl &> /dev/null; then
-  echo "Error: OpenSSL is not installed. Please install it and try again."
+  echo "Error: OpenSSL is required for this script. Install it using your package manager."
   exit 1
 fi
 
 # Function to display menu
 display_menu() {
+  banner
   echo "Select an option:"
   echo "----------------------"
   echo "[1] Encrypt a file"
   echo "[2] Decrypt a file"
   echo "[0] Exit"
   echo ""
-  read -p "Enter your choice: " choice
 }
 
 # Function to encrypt a file
 encrypt_file() {
   read -p "Enter the file to encrypt: " input_file
   if [ ! -f "$input_file" ]; then
-    echo "Error: File '$input_file' not found."
-    exit 1
+    echo ""
+    echo "File '$input_file' not found."
+    sleep 1.2
+    return
   fi
 
   output_file="${input_file}.enc"
@@ -55,13 +59,13 @@ encrypt_file() {
   openssl enc -aes-256-cbc -salt -in "$input_file" -out "$output_file" -pass pass:"$password" >/dev/null 2>&1
 
   if [ $? -eq 0 ]; then
-  	echo ""
-    echo -e "${GREEN}File Encrypted successfully -> $output_file"
-	echo ""
+    echo -e "${GREEN}File Encrypted successfully -> $output_file${NC}"
+    sleep 2
+    echo ""
     exit 1
   else
-    echo ""
-    echo "Error encrypting the file."
+    echo -e "${RED}Error encrypting the file.${NC}"
+    sleep 2
     echo ""
     exit 1
   fi
@@ -71,15 +75,14 @@ encrypt_file() {
 decrypt_file() {
   read -p "Enter the file to decrypt: " input_file
   if [ ! -f "$input_file" ]; then
-    echo "Error: File '$input_file' not found."
-    exit 1
+    echo "File '$input_file' not found."
+    sleep 1.2
+    return
   fi
 
   if [[ "$input_file" != *.enc ]]; then
-	echo ""
-    echo "Error: Input file does not have .enc extension. Cannot decrypt."
-	echo ""
-    exit 1
+    echo -e "${RED}Error: Input file does not have .enc extension. Cannot decrypt.${NC}"
+    return
   fi
 
   output_file="${input_file%.enc}"
@@ -88,14 +91,13 @@ decrypt_file() {
   openssl enc -aes-256-cbc -d -salt -in "$input_file" -out "$output_file" -pass pass:"$password" >/dev/null 2>&1
 
   if [ $? -eq 0 ]; then
-    echo ""
-    echo -e "${GREEN}File Decrypted successfully -> $output_file"
+    echo -e "${GREEN}File Decrypted successfully -> $output_file${NC}"
+    sleep 1.2
     echo ""
     exit 1
   else
-    echo ""
-    echo "Error decrypting the file. Please check the password."
-	echo ""
+    echo -e "${RED}Error decrypting the file. Please check the password.${NC}"
+    sleep 2
     exit 1
   fi
 }
@@ -103,6 +105,7 @@ decrypt_file() {
 # Main program
 while true; do
   display_menu
+  read -p "Enter your choice: " choice
   case $choice in
     1)
       encrypt_file
@@ -115,7 +118,8 @@ while true; do
       exit 0
       ;;
     *)
-      echo "Invalid choice. Please try again."
+      echo -e "${RED}Invalid choice. Please try again.${NC}"
+      sleep 1
       ;;
   esac
 done
