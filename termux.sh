98a56282f62a2d81e2280888b2c8293291b04a3e
kennn05 <bosslevelgod2@gmail.com>
2025-01-23 22:02:36 +0800

    k

--- termux.sh
+++ termux.sh
@@ -2,5 +2,5 @@ cp $HOME/filecrypt/filecrypt.sh $HOME/../usr/bin/filecrypt
 chmod +x $HOME/../usr/bin/filecrypt 
 clear 
 echo " SUCCESSFUL type 'filecrypt' to use the tool"
-echo "rm -f $HOME/../usr/bin/filecrypt && rm -rf $HOME/filecrypt && git clone https://github.com/kennn05/filecrypt $HOME/filecrypt && bash $HOME/filecrypt/termux.sh " > ~/../usr/bin/update
+echo "rm -f $HOME/../usr/bin/filecrypt && rm -rf $HOME/filecrypt && git clone https://github.com/kennn05/filecrypt && bash $HOME/filecrypt/termux.sh " > ~/../usr/bin/update
 chmod +x ~/../usr/bin/update
